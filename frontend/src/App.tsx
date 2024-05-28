d149ce9b8ae84d9a1082cf491b1b6ae208b5e271
raphael-gi <raphael.gisler@stud.edubs.ch>
2024-05-28 16:11:48 +0200

    Add shadcn-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button } from './components/ui/button';
 import './App.css'
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <Button>Hi</Button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
